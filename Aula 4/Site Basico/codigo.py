6ba0c109d2e8cab89449114a436040b08e9ebdc7
Anderson Domingos <anderson@mica12.com.br>
2023-09-29 22:59:27 -0300

    aula 4, site chat

---
+++ Aula 4/Site Basico/codigo.py
@@ -0,0 +1,78 @@
+#Hashzap
+# instalar o flet (integra o python com flutter) através do command prompt
+# criar um botão de iniciar chat
+# popup para entrar no site
+# quando entrar no chat (aparece para todo mundo)
+    # mensagem de entrada no site
+    # o campo e botão de enviar mensagem
+# a cada mensagem que você envia (aparece para todo mundo)
+    # Nome: Texto da Mensagem
+
+# para construir site com flet precisa de três passos:
+
+# 1 - importar o flet    
+import flet as ft
+
+# 2 - criar uma função no python que gerencia a página principal
+def main(pagina):
+    texto = ft.Text("Hashzap")
+    
+    chat = ft.Column()
+    
+    nome_usuario = ft.TextField(label="Escreva seu nome")
+    
+    def enviar_mensagem_tunel(mensagem):
+        tipo = mensagem["tipo"]
+        if tipo == "mensagem":
+            texto_mensagem = mensagem["texto"]
+            usuario_mensagem = mensagem["usuario"]
+            # adiciona mensagem no chat
+            chat.controls.append(ft.Text(f"{usuario_mensagem}: {texto_mensagem}"))
+        else:
+            usuario_mensagem = mensagem["usuario"]
+            chat.controls.append(ft.Text(f"{usuario_mensagem} entrou no chat", size=12, italic=True, color=ft.colors.ORANGE_500))
+        pagina.update()
+
+    pagina.pubsub.subscribe(enviar_mensagem_tunel)
+    
+    def enviar_mensagem(evento):
+        pagina.pubsub.send_all({"texto": campo_mensagem.value, "usuario": nome_usuario.value, 
+                                "tipo": "mensagem"})
+        # limpar o campo de mensagem
+        campo_mensagem.value = ""
+        pagina.update()
+    
+    campo_mensagem = ft.TextField(label="Digite uma mensagem", on_submit="enviar_mensagem")
+    botao_enviar_mensagem = ft.ElevatedButton("Enviar", on_click=enviar_mensagem)
+       
+    def entrar_popup(evento):
+        pagina.pubsub.send_all({"usuario": nome_usuario.value, "tipo": "entrada"})
+        pagina.add(chat)
+        popup.open = False
+        pagina.remove(botao_iniciar)
+        pagina.remove(texto)
+        pagina.add(ft.Row(
+            [campo_mensagem, botao_enviar_mensagem]
+        ))
+        pagina.update()
+    
+    popup = ft.AlertDialog(
+        open=False,
+        modal=True,
+        title=ft.Text("Bem-vindo ao Hashzap"),
+        content=nome_usuario,
+        actions=[ft.ElevatedButton("Entrar", on_click=entrar_popup)],
+    )
+        
+    def entrar_chat(evento):
+        pagina.dialog = popup
+        popup.open = True
+        pagina.update()
+        
+    botao_iniciar = ft.ElevatedButton("Iniciar", on_click=entrar_chat)
+    pagina.add(texto)
+    pagina.add(botao_iniciar)
+    
+
+# 3 - flet vai executar para chamar o aplicativo, passando a função como parâmetro
+ft.app(target=main, view=ft.WEB_BROWSER, port=8000)
